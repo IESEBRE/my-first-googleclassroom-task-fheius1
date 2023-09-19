16d35aef38b82fe298fcb78639ce84126fbf89ee
fheius1 <fheius@iesebre.com>
2023-09-19 13:15:12 +0200

    Primer projecte java del curs

---
+++ ProjecteProva/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+
+
+    int variable=9;
+
+    public static void main(String[] args) {
+        System.out.println("Que prefereixes?");
+        System.out.print("Lambo o Ferrari, ");
+        System.out.println("Pagani o Bugatti");
+    }
+}

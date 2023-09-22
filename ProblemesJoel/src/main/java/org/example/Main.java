c5f76e1b1802dc7e6f3d32e1d596301e11a75b60
fheius1 <145098835+fheius1@users.noreply.github.com>
2023-09-22 08:38:09 +0200

    Add files via upload

---
+++ ProblemesJoel/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner ent = new Scanner(System.in);
+        int edat= ent.nextInt();
+        edat = edat + 1;
+
+
+        System.out.println("L'any que ve tindras " + edat + " anys");
+    }
+}

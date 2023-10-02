f143a076ed0953723a0ecc4b84e5bfec2ba02581
fheius1 <145098835+fheius1@users.noreply.github.com>
2023-10-02 17:17:47 +0200

    Add files via upload

---
+++ ProblemesJoel/src/main/java/org/example/AviMesGran.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AviMesGran {
+
+    public static void main(String[] args) {
+        Scanner ent=new Scanner(System.in);
+
+        System.out.println("Com es diu lo primer avi?");
+        String nom1= ent.next();
+
+
+        System.out.println("I quants anys te?");
+        int edat1= ent.nextInt();
+
+        System.out.println("Com es diu lo segon avi?");
+        String nom2 = ent.next();
+
+        System.out.println("I quants anys te??");
+        int edat2 = ent.nextInt();
+
+        if (edat1 > edat2) System.out.println(nom1 + " te mes anys");
+        else if (edat1 < edat2) System.out.println(nom2 + " te mes anys");
+        else System.out.println("Tenen la mateixa edat");
+
+    }
+}

ac9ab2252e0758018a9b739dca308abeb55f1ba6
fheius1 <fheius@iesebre.com>
2023-10-04 11:41:26 +0200

    ProblemesiExemples2

--- ProblemesJoel/src/main/java/org/example/ContarNotes.java
+++ ProblemesJoel/src/main/java/org/example/ContarNotes.java
@@ -7,16 +7,18 @@ public class ContarNotes {
     public static void main(String[] args) {
 
         Scanner ent = new Scanner(System.in);
-        int num1= ent.nextInt();
+        int num1= 0;
         int comptarnumeros = 0; // en este cas comencem a comptar desde 0 i
         int valortotal=0;
         int  cE,cN, cB, cS, cI, cM;
         cE=cN= cB= cS= cI= cM= 0;
 
         while (num1 != -1){
+            num1= ent.nextInt();
 
             if (num1 >= 0 && num1 <= 10){ // nomes sumo i compto els valors entre 0 i 10
                 valortotal =valortotal + num1;
+                comptarnumeros++;
                 switch (num1){
                     case 10:
                     case 9:
@@ -39,14 +41,18 @@ public class ContarNotes {
                     case 2:
                     case 1:
                     case 0:
+                        cM++;
                 }
-                comptarnumeros++;
-            }
-            num1= ent.nextInt();
 
+            }
 
         }
-        System.out.println();
+
+        System.out.println("NOTES: "+comptarnumeros+" MITJANA: "+((float)valortotal/comptarnumeros)+" E: "+cE +" N: "+cN+" B: "+cB+" S: "+cS+" I: "+cI+" MD: "+cM);
+        //al ficar float entre parentesis estem fent un casting
+        //per a que la divisio pugui donar amb decimal
+
+       // System.out.format(" NOTES: %d MITJANA: %f E: %d N: %d B: %d S: %d I: %d MD: %d", comptarnumeros, (float)valortotal/comptarnumeros, cE, cN, cB, cS, cI, cM);
 
     }
 }
---
+++ ProblemesJoel/src/main/java/org/example/Exercici1Moodle.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Exercici1Moodle {
+
+    public static void main(String[] args) {
+
+        int num1 = -50;
+        while (num1 <=50) { // la condicio al inici te que ser true pero acabar en false per a no ser un bucle infinit
+            System.out.println(num1);
+            num1 = num1 + 1;
+
+        }
+    }
+}

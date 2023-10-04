db48f16d5ce37b5ab380060e5680f554d3684d43
fheius1 <fheius@iesebre.com>
2023-10-04 12:15:02 +0200

    ProblemesiExemples3

--- ProblemesJoel/src/main/java/org/example/Exercici1Moodle.java
+++ ProblemesJoel/src/main/java/org/example/Exercici1Moodle.java
@@ -1,14 +1,60 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Exercici1Moodle {
 
     public static void main(String[] args) {
 
-        int num1 = -50;
-        while (num1 <=50) { // la condicio al inici te que ser true pero acabar en false per a no ser un bucle infinit
-            System.out.println(num1);
+        Scanner ent = new Scanner(System.in);
+
+
+        System.out.println("Introdueix dos numeros");
+        int num1 = ent.nextInt();
+        int num2 = ent.nextInt();
+        int valormesgran = 0;
+
+        while (num1 <= num2) {
+            if (num1 > num2) {
+                System.out.println(num1 % 2 == 0 ? num1 + " es parell" : num1 + " es imparell"); //Utilitzant lo metode ternari
+            }
+            else (num2 <= num1) {
+                valormesgran = num2;
+                System.out.println(valormesgran % 2 == 0 ? valormesgran + " es parell" : valormesgran + " es imparell"); //Utilitzant lo metode ternari
+            }
             num1 = num1 + 1;
 
         }
     }
 }
+
+
+
+
+/*Mostrar una serie de numeros i di si es parell o imparell
+int num1 = -50;
+while (num1 <=50) {
+System.out.println(num1 % 2 == 0 ? num1 + " es parell" : num1 + " es imparell"); //Utilitzant lo metode ternari
+num1 = num1 + 1;
+}
+ */
+
+
+
+
+/*Fer que l'usuari demani dos numeros en ordre creixent i dir quins son parells i imparells
+   Scanner ent = new Scanner(System.in);
+System.out.println("Introdueix dos numeros");
+int num1 = ent.nextInt();
+int num2 = ent.nextInt();
+while (num1 <= num2) {
+System.out.println(num1 % 2 == 0 ? num1 + " es parell" : num1 + " es imparell"); //Utilitzant lo metode ternari
+num1 = num1 + 1;
+}
+}
+ */
+
+
+/*Fer que l'usuari demani dos numeros en qualsevol ordre i dir quins son parells i imparells
+
+ */
